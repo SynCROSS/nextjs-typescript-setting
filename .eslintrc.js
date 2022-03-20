338231ee71333aa45f0b39755473cf2aaa95cb19
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2022-03-20 14:13:31 +0900

    Update ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
 module.exports = {
   extends: [
     'next/core-web-vitals',
-    'airbnb-base',
-    './node_modules/eslint-config-airbnb-base/rules/best-practices.js',
-    './node_modules/eslint-config-airbnb-base/rules/errors.js',
-    './node_modules/eslint-config-airbnb-base/rules/node.js',
-    './node_modules/eslint-config-airbnb-base/rules/style.js',
-    './node_modules/eslint-config-airbnb-base/rules/variables.js',
-    './node_modules/eslint-config-airbnb-base/rules/es6.js',
-    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
     'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
     project: './tsconfig.json',
   },
   plugins: ['@typescript-eslint'],
   rules: {
-    'lines-between-class-members': 'off',
+    'lines-between-class-members': 'warn',
     '@typescript-eslint/no-for-in-array': 'warn',
     '@typescript-eslint/explicit-function-return-type': [
       'warn',
@@ -25,6 +25,19 @@ module.exports = {
         allowExpressions: true,
       },
     ],
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'react/prefer-stateless-function': 'warn',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    ],
+    'react/jsx-one-expression-per-line': 'warn',
+    'react/jsx-props-no-spreading': 'warn',
+    'no-param-reassign': 'warn',
+    'no-nested-ternary': 'warn',
     'import/extensions': [
       'error',
       'ignorePackages',
@@ -38,6 +51,7 @@ module.exports = {
   },
   env: {
     browser: true,
+    es2021: true,
     node: true,
   },
 };
